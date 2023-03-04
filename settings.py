1ba8b978642d45185d5331bcd58750879673b31a
lhuisenga <98440150+lhuisenga@users.noreply.github.com>
2023-03-04 16:11:38 -0600

    fixed settings

--- settings.py
+++ settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-26x_x^f((ghwtbb&_5z(nkx&a#hzku@!q%n+oxb^(h)n^v(xp8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
